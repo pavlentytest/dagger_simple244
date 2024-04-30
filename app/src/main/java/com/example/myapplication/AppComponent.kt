aeb2458088311740d0662552ee4d11b765ebf39e
Pavel Stepanov <impetus@yandex.ru>
2024-04-30 18:35:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/AppComponent.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.database.DatabaseUtils
+import com.example.myapplication.module.DatabaseModule
+import com.example.myapplication.module.NetworkModule
+import dagger.Component
+
+
+@Component(modules=[DatabaseModule::class,NetworkModule::class])
+interface AppComponent {
+    fun getDb() : DatabaseHelper
+    fun getNt() : NetworkUtility
+
+    fun injectToMainActivity(mainActivity: MainActivity)
+}
